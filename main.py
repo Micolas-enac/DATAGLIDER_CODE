2c38853b177891dd6e27ac14f8716ecb65c92248
richarju <48989838+richarju@users.noreply.github.com>
2020-03-25 16:05:37 +0100

    modification commentaire nicolas dans main

--- main.py
+++ main.py
@@ -5,7 +5,8 @@ import geopandas
 import numpy as np
 import data_analysis
 
-"Nicolas rajoute un commentaire"
+"Nicolas rajoute un commentaire que jules modifie"
+
 def make_list_of_tracks(repertoire, list_of_names_txt):
     list_filename = "IGC_FILES/" + repertoire + "/" + list_of_names_txt
     file = open(list_filename, 'r')
